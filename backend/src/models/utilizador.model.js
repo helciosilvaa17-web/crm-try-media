1f8be42f58c00552f80f2479059b297b1953fc10
helciosilvaa17-web <helciosilvaa17@gmail.com>
2026-05-04 01:28:03 +0100

    Encontrar utilizadores e criar utilizadores no ModelUtilizador

--- backend/src/models/utilizador.model.js
+++ backend/src/models/utilizador.model.js
@@ -1,1 +1,14 @@
 const db = require('../config/db')
+
+const UtilizadorModel = {
+    encontrarPorEmail: async (email) => {
+        const [rows] = await db.query('SELECT * FROM utilizadores WHERE email = ?', [email])
+        return rows [0]
+    },
+
+    criar: async (nome, email, hash, perfil) => {
+        await db.query('INSERT INTO utilizadores (nome, email, palavra_passe, perfil) VALUES (?, ?, ?, ?)', [nome, email, hash, perfil])
+    },
+
+    
+}

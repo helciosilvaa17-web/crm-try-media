6722829646402d92a306b78186718721872c5ee1
helciosilvaa17-web <helciosilvaa17@gmail.com>
2026-05-04 02:24:47 +0100

    Criação da rota clientes

---
+++ backend/src/routes/clientes.routes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router  = express.Router()
+const ClientesController = require('../controllers/clientes.controller')
+const { autenticar, apenasAdmin } = require('../middlewares/auth')
+
+router.use(autenticar)
+
+router.get('/',                    ClientesController.listar)
+router.get('/:id',                 ClientesController.obter)
+router.post('/',                   ClientesController.criar)
+router.put('/:id',                 ClientesController.atualizar)
+router.delete('/:id', apenasAdmin, ClientesController.apagar)
+router.post('/:id/interacoes',     ClientesController.criarInteracao)
+
+module.exports = router

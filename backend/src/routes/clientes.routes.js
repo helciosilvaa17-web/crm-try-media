6c065b7486e556304b73d04c10ed073b63e07d66
helciosilvaa17-web <helciosilvaa17@gmail.com>
2026-05-15 00:29:30 +0100

    Update clientes.routes.js

--- backend/src/routes/clientes.routes.js
+++ backend/src/routes/clientes.routes.js
@@ -5,11 +5,13 @@ const { autenticar, apenasAdmin } = require('../middlewares/auth')
 
 router.use(autenticar)
 
-router.get('/',                    ClientesController.listar)
-router.get('/:id',                 ClientesController.obter)
-router.post('/',                   ClientesController.criar)
-router.put('/:id',                 ClientesController.atualizar)
-router.delete('/:id', apenasAdmin, ClientesController.apagar)
-router.post('/:id/interacoes',     ClientesController.criarInteracao)
+router.get('/',                             ClientesController.listar)
+router.get('/:id',                          ClientesController.obter)
+router.post('/',                            ClientesController.criar)
+router.put('/:id',                          ClientesController.atualizar)
+router.delete('/:id', apenasAdmin,          ClientesController.apagar)
+router.post('/:id/interacoes',              ClientesController.criarInteracao)
+router.put('/:clienteId/interacoes/:id',    ClientesController.editarInteracao)
+router.delete('/:clienteId/interacoes/:id', ClientesController.apagarInteracao)
 
 module.exports = router
